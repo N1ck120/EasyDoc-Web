7097f88d8c4afcbd9108eb2aba95699c017b7215
N1ck120 <nickolasaugusto74@gmail.com>
2025-03-26 10:16:15 -0300

    Initial Commit

---
+++ EasyDoc/src/main/java/com/easydoc/EasyDoc/EasyDocApplication.java
@@ -0,0 +1,13 @@
+package com.easydoc.EasyDoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyDocApplication.class, args);
+	}
+
+}
---
+++ EasyDoc/src/test/java/com/easydoc/EasyDoc/EasyDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easydoc.EasyDoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
